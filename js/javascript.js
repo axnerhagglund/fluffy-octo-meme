7eeb99c9d4aafc9c1555e4cf326c84977a291a38
Axner <@axnerhagglund@gmail.com>
2024-12-03 16:05:58 +0100

    added a border on the navbar. merged and deleted some branches, added a new page where the confirmations will show, and did some javascript to add functionality to the page

--- js/javascript.js
+++ js/javascript.js
@@ -12,8 +12,31 @@ function openOverlay(){
 
 }
 
+// stänger overlay genom att göra bredden på overlayn till 0
 function closeOverlay(){
     document.getElementById("navOverlay").style.width = "0";
 }
+
+
+//byter text på book now! till reserverad samt lagt till en paragraf under
+// så att kunden enkelt kan se vad hen har gjort samt få reda på vart hen kan se sina bokningar.
+
+let itsBooked = document.getElementById("card_btn")
+let cardConfirmationMessage = document.getElementById("card__confirmation__message")
+
+
+function bookedBtn(){
+    itsBooked.textContent = "reserved";
+    itsBooked.disabled = true;
+    itsBooked.style.backgroundColor = "#fff";
+    itsBooked.style.color = "#000";
+    itsBooked.style.fontWeight = "700";
+    cardConfirmationMessage.textContent = "click here to see your reservations";
+    cardConfirmationMessage.style.color = "green"
+    cardConfirmationMessage.style.padding = ".7rem 0 0 0"
+}
+
+
+
 
 
